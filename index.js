a9eacff7d3433872d59f2d5fef54ce88d4913e3e
Anna-Miron <ana04miron@gmail.com>
2024-10-03 18:01:16 +0100

    add loops

--- index.js
+++ index.js
@@ -87,3 +87,108 @@ switch(weekDay) {
         console.log('No specific day was found'); 
         break;                        
 }
+
+// while
+// calcularea sumei totale dintr-un cos de cumparatauri folosind "while"
+
+const cartItems = [
+    {
+        name: "Iphone 16",
+        price: 1700,
+        curency: "$",
+        amount: 1
+    },
+    {
+        name: "Protector sleeve",
+        price: 100,
+        curency: "$",
+        amount: 3
+    },
+    {
+        name: "Apple sticker",
+        price: 21.99,
+        curency: "$",
+        amount: 4
+    },
+]
+
+let totalPrice = 0;
+let productIndex = 0;
+while (productIndex < cartItems.length) {
+    let product = cartItems[productIndex];
+    totalPrice += product.price * product.amount;
+    // totalPrice = totalPrice + product.price * product.amount;
+    productIndex++;
+    // productIndex = productIndex + 1;
+}
+
+console.log("Your cart total is: ", totalPrice, cartItems[0].curency);
+
+// do - while
+
+let n = 6;
+let currentNUmber = 0;
+do {
+    currentNUmber++;
+    console.log(currentNUmber);
+} while (currentNUmber < n);
+
+// for
+// calcularea sumei totale dintr-un cos de cumparatauri folosind "for"
+let totalPrice2 = 0;
+for (i = 0; i < cartItems.length; i++) {
+    let product = cartItems[i];
+    totalPrice2 += product.price * product.amount;
+}
+
+console.log("Your cart total computed with 'for' is: ", totalPrice2, cartItems[0].curency);
+
+// for of
+// calcularea sumei totale dintr-un cos de cumparatauri folosind "for of"
+let totalPrice3 = 0;
+for (let product of cartItems) {
+    totalPrice3 += product.amount * product.price;
+}
+
+console.log("Your cart total computed with 'for of' is: ", totalPrice3, cartItems[0].curency);
+
+//for in 
+let iphone = {
+    name : 'Iphone 16',
+    price : 1700,
+    currency : '£',
+    amount: 1
+};
+
+for (let key in iphone) {
+    console.log('key: ', key);
+}
+
+//break
+
+const numbers = [123, 43, 12, 12312312, 9];
+
+let numberToCheck = 12;
+
+for (let i = 0; i < numbers.length; i++) {
+    console.log('i: ', i)
+    if (numberToCheck === numbers[i]) {
+        console.log('Number ', numberToCheck, 'is found at index', i);
+        break;
+    }
+    console.log('i: ', i);
+}
+
+//continue
+// calcularea sumei promelor n numere pare
+
+let firstN = 10;
+let sum = 0;
+for (i = 1; i <= firstN; i++) {
+    if (i % 2 !== 0) {
+        continue;
+    }
+    sum += i;
+
+}
+console.log('Suma primelor ', firstN, 'numere pare este: ', sum);
